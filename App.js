24901c3c9fa1c13ac14f3d14d4120cce670b015f
Sai Krishna Reddy Katta <saikrishnareddykatta@Sais-MacBook-Pro.local>
2023-11-14 17:47:55 -0800

    feat(gamescreen): added game logic and screen

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StyleSheet, ImageBackground } from "react-native";
+import { useState } from "react";
+import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
+import Colors from "./constants/colors";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState(null);
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (userNumber) {
+    screen = <GameScreen userNumber={userNumber} />;
+  }
+
   return (
-    <LinearGradient style={styles.rootScreen} colors={["#4e0329", "#ddb52f"]}>
+    <LinearGradient
+      style={styles.rootScreen}
+      colors={[Colors.primary700, Colors.accent500]}
+    >
       <ImageBackground
         source={require("./assets/images/background.png")}
         resizeMode="cover" // it makes sure the image is always covered up with the available space
@@ -12,7 +30,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        <StartGameScreen />
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
--- components/ui/PrimaryButton.js
+++ components/ui/PrimaryButton.js
@@ -1,4 +1,5 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
 
 function PrimaryButton({ children, onPress }) {
   function presshandler() {
@@ -8,7 +9,7 @@ function PrimaryButton({ children, onPress }) {
     <View style={styles.buttonOuterContainer}>
       <Pressable
         onPress={presshandler}
-        android_ripple={{ color: "#640202" }}
+        android_ripple={{ color: Colors.primary600 }}
         style={(pressData) =>
           pressData.pressed
             ? [styles.buttonInnerContainer, styles.pressed]
@@ -30,7 +31,7 @@ const styles = StyleSheet.create({
     overflow: "hidden", // it clips the css elements when it overflows from the element
   },
   buttonInnerContainer: {
-    backgroundColor: "#72063c",
+    backgroundColor: Colors.primary500,
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
